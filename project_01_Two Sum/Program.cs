1b991f67b6e81a05d59da721c14dac80f8975242
Alexander <sany199574@gmail.com>
2024-04-04 03:10:58 +0300

    Добавлено решение задачи - TwoSum

--- project_01_Two Sum/Program.cs
+++ project_01_Two Sum/Program.cs
@@ -1,5 +1,25 @@
 ﻿
+int[] array = new int[] { 2, 7, 11, 15 };
+int target = 9;
 
-
+int[] result = TwoSum(array, target);
 
 Console.ReadLine();
+
+
+static int[] TwoSum(int[] nums, int target)
+{
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i+1; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                return new int[2] { i, j };
+            }
+        }
+    }
+
+    return new int[2] { 0,1 };
+}
