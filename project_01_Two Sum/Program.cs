d5b988b6a7f8be524e5ac0d410076c844d60b4dd
Alexander <sany199574@gmail.com>
2024-04-04 03:21:49 +0300

    Добавлены дополнительные проверки для задания

--- project_01_Two Sum/Program.cs
+++ project_01_Two Sum/Program.cs
@@ -1,8 +1,16 @@
 ﻿
-int[] array = new int[] { 2, 7, 11, 15 };
-int target = 9;
+int[] array1 = new int[] { 2, 7, 11, 15 };
+int target1 = 9;
 
-int[] result = TwoSum(array, target);
+int[] array2 = new int[] { 3, 2, 4 };
+int target2 = 6;
+
+int[] array3 = new int[] { 3, 3 };
+int target3 = 6;
+
+int[] result1 = TwoSum(array1, target1);
+int[] result2 = TwoSum(array2, target2);
+int[] result3 = TwoSum(array3, target3);
 
 Console.ReadLine();
 
