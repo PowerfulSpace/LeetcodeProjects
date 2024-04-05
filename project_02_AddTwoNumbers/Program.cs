f4c3c11584f761cb6d5a81b2e9bc82edbd018856
Alexander <sany199574@gmail.com>
2024-04-05 15:46:55 +0300

    Добавлено задание AddTwoNumbers

---
+++ project_02_AddTwoNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+List<int> array1 = new List<int>() { 2, 4, 3 };
+List<int> array2 = new List<int>() { 5, 6, 4 };
+
+LinkedList<int> l1 = new LinkedList<int>(array1);
+LinkedList<int> l2 = new LinkedList<int>(array2);
+
+
+AddTwoNumbers(l1,l2);
+
+
+Console.ReadLine();
+
+static LinkedList<int> AddTwoNumbers(LinkedList<int> l1, LinkedList<int> l2)
+{
+
+}
