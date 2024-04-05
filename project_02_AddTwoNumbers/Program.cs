ca9a7c732eb0769c5021441cbc6f585410d440be
Alexander <sany199574@gmail.com>
2024-04-05 15:58:43 +0300

    Добавлен метод Print

--- project_02_AddTwoNumbers/Program.cs
+++ project_02_AddTwoNumbers/Program.cs
@@ -6,8 +6,11 @@ List<int> array2 = new List<int>() { 5, 6, 4 };
 LinkedList<int> l1 = new LinkedList<int>(array1);
 LinkedList<int> l2 = new LinkedList<int>(array2);
 
+Print(l1);
+Print(l2);
 
-AddTwoNumbers(l1,l2);
+
+AddTwoNumbers(l1, l2);
 
 
 Console.ReadLine();
@@ -15,4 +18,13 @@ Console.ReadLine();
 static LinkedList<int> AddTwoNumbers(LinkedList<int> l1, LinkedList<int> l2)
 {
 
+}
+
+static void Print(LinkedList<int> link)
+{
+	foreach (var item in link)
+	{
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
 }
