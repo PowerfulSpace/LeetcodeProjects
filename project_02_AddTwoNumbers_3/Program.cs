83754fead04812b6ac8e523fbbcb6f0b2341d1c9
Alexander <sany199574@gmail.com>
2024-04-06 23:54:21 +0300

    Удалён лишний проект

--- project_02_AddTwoNumbers_3/Program.cs
+++ project_02_AddTwoNumbers_3/Program.cs
@@ -20,24 +20,24 @@ Console.ReadLine();
 
 static LinkedListNode<int> AddTwoNumbers(LinkedListNode<int> l1, LinkedListNode<int> l2)
 {
-    int sum = l1.val + l2.val;
-    var result = new ListNode(sum % 10, null);
-    ListNode current = result;
-    l1 = l1?.next;
-    l2 = l2?.next;
+    int sum = l1.Value + l2.Value;
+    LinkedListNode<int> result = new LinkedListNode<int>(sum % 10);
+    LinkedListNode<int> current = result;
+    l1 = l1?.Next;
+    l2 = l2?.Next;
     int firstVal;
     int secondVal;
     while (l1 != null || l2 != null || sum >= 10)
     {
-        firstVal = l1 != null ? l1.val : 0;
-        secondVal = l2 != null ? l2.val : 0;
+        firstVal = l1 != null ? l1.Value : 0;
+        secondVal = l2 != null ? l2.Value : 0;
         sum = sum < 10 ? firstVal + secondVal : firstVal + secondVal + 1;
 
-        current.next = new ListNode(sum % 10, null);
-        current = current.next;
+        current.Next = new LinkedListNode<int>(sum % 10);
+        current = current.Next;
 
-        l1 = l1?.next;
-        l2 = l2?.next;
+        l1 = l1?.Next;
+        l2 = l2?.Next;
     }
     return result;
 }
