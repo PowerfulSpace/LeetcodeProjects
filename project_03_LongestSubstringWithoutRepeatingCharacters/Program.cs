daa005a754d9784796c0264a9d42d945b4787170
Alexander <sany199574@gmail.com>
2024-04-07 01:38:44 +0300

    Добавлено тестовое решение

--- project_03_LongestSubstringWithoutRepeatingCharacters/Program.cs
+++ project_03_LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -1,1 +1,44 @@
-﻿Console.ReadLine();
+﻿string input = "abcabcbb";
+
+//int result = LengthOfLongestSubstring(input);
+//Console.WriteLine(result);
+
+int result = Method(input);
+Console.WriteLine();
+Console.WriteLine(result);
+
+Console.ReadLine();
+
+
+
+static int Method(string s)
+{
+    List<char> chars = new List<char>();
+    int count = 0;
+    int maxCount = 0;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        Console.Write(s[i]);
+
+        if (chars.Contains(s[i]))
+        {
+            count = 0;
+        }
+        else
+        {
+            chars.Add(s[i]);
+            count++;
+
+            if (count >= maxCount)
+            {
+                maxCount = count;
+            }  
+        }
+    }
+    return maxCount;
+}
+
+//static int LengthOfLongestSubstring(string s)
+//{
+//}
