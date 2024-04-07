2aeb45720abae1dc50909cfe03f4638c9a62b51a
Alexander <sany199574@gmail.com>
2024-04-07 19:52:17 +0300

    Добавлен новый алгоритм для решения задачи LengthOfLongestSubstring

---
+++ project_03_LongestSubstringWithoutRepeatingCharacters_3/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+string input1 = "abcabcbb";
+string input2 = "bbbbb";
+string input3 = "pwwkew";
+
+Console.WriteLine(LengthOfLongestSubstring(input1));
+Console.WriteLine(LengthOfLongestSubstring(input2));
+Console.WriteLine(LengthOfLongestSubstring(input3));
+
+
+Console.ReadLine();
+
+
+
+static int LengthOfLongestSubstring(string s)
+{
+    Queue<char> subs = new Queue<char>();
+    int res = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (subs.Contains(s[i]) == false)
+        {
+            subs.Enqueue(s[i]);
+        }
+        else
+        {
+            char need = s[i];
+            res = Math.Max(res, subs.Count);
+            while (subs.Peek() != s[i])
+            {
+                subs.Dequeue();
+            }
+            subs.Dequeue();
+            subs.Enqueue(need);
+        }
+    }
+    res = Math.Max(res, subs.Count);
+    return res;
+}
