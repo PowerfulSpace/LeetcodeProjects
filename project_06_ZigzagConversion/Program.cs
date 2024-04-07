2e29a78af0af60f1e1824ffb554b28cc74db542a
Alexander <sany199574@gmail.com>
2024-04-08 02:14:47 +0300

    Добавлено условие в пример

--- project_06_ZigzagConversion/Program.cs
+++ project_06_ZigzagConversion/Program.cs
@@ -1,3 +1,14 @@
 ﻿
 
+string input = "PAYPALISHIRING";
+int rows = 4;
+
+Console.WriteLine(Convert(input,rows));
+
 Console.ReadLine();
+
+
+static string Convert(string s, int numRows)
+{
+    return "1";
+}
