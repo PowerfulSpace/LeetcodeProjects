e16a88c4e953a384c9c55dcab945a2e23759225c
Alexander <sany199574@gmail.com>
2024-04-09 02:02:42 +0300

    Добавлена реализация задания

--- project_06_ZigzagConversion/Program.cs
+++ project_06_ZigzagConversion/Program.cs
@@ -4,7 +4,7 @@ using System;
 using System.Text;
 
 string input = "PAYPALISHIRING";
-int rows = 4;
+int rows = 5;
 
 var result = Convert(input, rows);
 Console.WriteLine();
@@ -16,13 +16,93 @@ Console.ReadLine();
 
 static string Convert(string s, int numRows)
 {
-    int column = (int)Math.Ceiling((decimal)numRows / 2);
-    int rows = 0;
-    char[,] chars = new char[numRows + (numRows / 2),];
-    
+    int addChars = (int)Math.Ceiling((decimal)numRows / 2);
+    int countCharsEmpty = (numRows - 1) * addChars;
+
+    StringBuilder sb = new StringBuilder(s.Length * 2);
+    int index = 0;
 
 
-    return s;
+    if (numRows >= 4)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (index % numRows == 0 && i != 0)
+            {
+                index = 0;
+
+                sb.Append('-');
+                sb.Append(s[i]);
+                i++;
+                for (int j = 0; j < numRows; j++)
+                {
+                    sb.Append('-');
+                }
+                sb.Append(s[i]);
+                i++;
+                sb.Append('-');
+            }
+            if (i < s.Length)
+            {
+                index++;
+                sb.Append(s[i]);
+            }
+        }
+    }
+    else
+    {
+        if (numRows == 3)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (index % numRows == 0 && i != 0)
+                {
+                    index = 0;
+
+                    sb.Append('-');
+                    sb.Append(s[i]);
+                    i++;
+                    sb.Append('-');
+                }
+                if (i < s.Length)
+                {
+                    index++;
+                    sb.Append(s[i]);
+                }
+            }
+        }
+        if(numRows == 2)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (index % numRows == 0 && i != 0)
+                {
+                    index = 0;
+
+                    sb.Append(s[i+1]);
+                    sb.Append(s[i]);
+                    i += 2;
+                }
+                if (i < s.Length)
+                {
+                    index++;
+                    sb.Append(s[i]);
+                }
+            }
+        }
+        if (numRows == 1)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                sb.Append(s[i]);
+            }
+        }
+    }
+
+
+
+
+    return sb.ToString();
 }
 
 
