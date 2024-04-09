126a1ea423c340b3515164e91c1a657a232dc7c8
Alexander <sany199574@gmail.com>
2024-04-09 07:03:08 +0300

    Добавлено тестовое решение

---
+++ project_06_ZigzagConversion_2/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+string input = "PAYPALISHIRING";
+int rows = 3;
+
+var result = Convert(input, rows);
+Console.WriteLine(result);
+
+Console.ReadLine();
+
+static string Convert(string s, int numRows)
+{
+    string[] rows = new string[numRows];
+
+    int row = 0;
+    bool directionDown = true;
+
+    foreach (char item in s)
+    {
+        rows[row] += item.ToString();
+
+        directionDown = row == 0 ? true : row == numRows - 1 ? false : directionDown;
+
+        row = directionDown ? row + 1 : row - 1;
+
+    }
+    string output = "";
+
+    foreach (var item in rows)
+    {
+        output += item;
+    }
+
+
+    return output;
+}
