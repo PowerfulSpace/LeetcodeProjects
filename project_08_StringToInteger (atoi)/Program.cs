ebb56e304a5806b599ddc72957abeef66c102dcc
Alexander <sany199574@gmail.com>
2024-04-10 17:58:08 +0300

    Добавлено решение алгоритма

---
+++ project_08_StringToInteger (atoi)/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+using System.Text.RegularExpressions;
+
+string input1 = "42";
+string input2 = "   -42";
+string input3 = "4193 with words";
+string input4 = "words and 987";
+
+//Console.WriteLine(MyAtoi(input1));
+//Console.WriteLine(MyAtoi(input2));
+//Console.WriteLine(MyAtoi(input3));
+Console.WriteLine(MyAtoi(input4));
+
+Console.ReadLine();
+
+
+static int MyAtoi(string s)
+{
+
+    string[]  elements = s.Split(' ');
+
+    string pattern = @"-?\d+";
+    Regex regex = new Regex(pattern);
+
+    Match match = null;
+
+    foreach (string element in elements)
+    {
+        if (regex.IsMatch(element))
+        {
+            match = regex.Match(s);
+        }
+    }
+
+    int result = 0;
+    if (match.Result != null)
+    {
+        result = int.Parse(match.Value);
+    }
+
+    
+
+    return result;
+}
