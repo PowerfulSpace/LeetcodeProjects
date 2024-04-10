4612e06ecc64309f99b07594357ce03924e9394b
Alexander <sany199574@gmail.com>
2024-04-10 11:14:53 +0300

    Добавлено новое решение

--- project_07_ReverseInteger/Program.cs
+++ project_07_ReverseInteger/Program.cs
@@ -1,15 +1,49 @@
 ﻿
 
-int input = 123;
+using System.Text;
 
-Console.WriteLine(Reverse(input));
+int input1 = 123;
+int input2 = -123;
+
+Console.WriteLine(Reverse(input1));
+Console.WriteLine(Reverse(input2));
 
 Console.ReadLine();
 
 
 static int Reverse(int x)
 {
-    return 1;
+    bool negative = false;
+
+    if (x < 0)
+        negative = true;
+
+    string digit = x.ToString();
+
+
+    var b = digit.Reverse();
+    StringBuilder sb = new StringBuilder();
+
+    foreach (var item in b)
+    {
+        sb.Append(item);
+    }
+
+    if (negative == true)
+    {
+        sb.Remove(sb.Length - 1, 1);
+    }
+   
+
+    int result = int.Parse(sb.ToString());
+
+    if (negative == true)
+    {
+        result -= (result * 2);
+    }
+
+    return result;
 }
+
 
 
