daf1c0a73162fc75796427d4bb76fcd5d78fa8dc
Alexander <sany199574@gmail.com>
2024-04-10 13:15:59 +0300

    Добавлен пример

---
+++ project_07_ReverseInteger_5/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+
+int input1 = 123;
+int input2 = -123;
+int input3 = 1534236469;
+
+Console.WriteLine(Reverse(input1));
+Console.WriteLine(Reverse(input2));
+Console.WriteLine(Reverse(input3));
+
+Console.ReadLine();
+
+static int Reverse(int x)
+{
+    List<char> digits = x.ToString().ToList();
+    digits.Reverse();
+    bool negative = false;
+    foreach (char a in digits)
+    {
+        if (a == '-')
+        {
+            negative = true;
+
+        }
+    }
+    digits = digits.FindAll(x => x != '-');
+    Console.WriteLine(string.Join("", digits.ToArray()));
+
+    int value = 0;
+    try
+    {
+        value = negative ? int.Parse(string.Join("", digits.ToArray())) * -1 : int.Parse(string.Join("", digits.ToArray()));
+    }
+    catch
+    {
+
+    }
+    return value >= int.MaxValue || value <= int.MinValue ? 0 : value;
+}
+
+
+
