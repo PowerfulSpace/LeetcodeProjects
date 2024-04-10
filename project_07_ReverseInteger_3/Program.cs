eaa4871c1f5db06128e4e8c66732fb7104ce4790
Alexander <sany199574@gmail.com>
2024-04-10 12:54:09 +0300

    Добавлена реализация алгоритма для реверсирования

---
+++ project_07_ReverseInteger_3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+int input1 = 123;
+int input2 = -123;
+int input3 = 1534236469;
+
+Console.WriteLine(Reverse(input1));
+Console.WriteLine(Reverse(input2));
+Console.WriteLine(Reverse(input3));
+
+Console.ReadLine();
+
+static int Reverse(int x)
+{
+    string strReverse = new string(x.ToString().Trim('-').Reverse().ToArray());
+    bool tryInt32 = int.TryParse(strReverse, out int intVal);
+    return tryInt32 ? x.ToString().Contains("-") ? intVal * -1 : intVal : 0;
+}
+
+
+
