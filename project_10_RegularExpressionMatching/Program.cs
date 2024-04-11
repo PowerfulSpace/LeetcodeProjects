65a83ebac705498967a8c76037a47bb2d2f48eee
Alexander <sany199574@gmail.com>
2024-04-11 17:51:44 +0300

    Выполнена задача

--- project_10_RegularExpressionMatching/Program.cs
+++ project_10_RegularExpressionMatching/Program.cs
@@ -1,4 +1,18 @@
 ﻿
+using System.Text.RegularExpressions;
+
+string input1 = "aa";
+string pattern1 = "a";
+
+string input2 = "aa";
+string pattern2 = "a*";
+
+string input3 = "ab";
+string pattern3 = ".*";
+
+Console.WriteLine(IsMatch(input1,pattern1));
+Console.WriteLine(IsMatch(input2,pattern2));
+Console.WriteLine(IsMatch(input3,pattern3));
 
 
 Console.ReadLine();
@@ -6,5 +20,15 @@ Console.ReadLine();
 
 static bool IsMatch(string s, string p)
 {
-    return true;
+    Regex regex = new Regex(p);
+    Match result = regex.Match(s);
+
+    if(result.Value == s)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
