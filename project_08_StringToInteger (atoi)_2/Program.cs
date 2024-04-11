66cd2e74a362e6fa78a8403e3e9059816317d382
Alexander <sany199574@gmail.com>
2024-04-11 15:49:47 +0300

    Добавлен проанализированный новый алгоритм

--- project_08_StringToInteger (atoi)_2/Program.cs
+++ project_08_StringToInteger (atoi)_2/Program.cs
@@ -1,67 +1,69 @@
 ﻿
-
-
-using System.Text.RegularExpressions;
-using System.Xml.Linq;
-
 string input1 = "42";
 string input2 = "   -42";
 string input3 = "4193 with words";
 string input4 = "words and 987";
 string input5 = ".1";
 string input6 = "w1";
+string input7 = "1w23";
+string input8 = "987419341934193";
 
-Console.WriteLine(MyAtoi(input1));
-Console.WriteLine(MyAtoi(input2));
-Console.WriteLine(MyAtoi(input3));
-Console.WriteLine(MyAtoi(input4));
-Console.WriteLine(MyAtoi(input5));
-Console.WriteLine(MyAtoi(input6));
+//Console.WriteLine(MyAtoi(input1));
+//Console.WriteLine(MyAtoi(input2));
+//Console.WriteLine(MyAtoi(input3));
+//Console.WriteLine(MyAtoi(input4));
+//Console.WriteLine(MyAtoi(input5));
+//Console.WriteLine(MyAtoi(input7));
+Console.WriteLine(MyAtoi(input8));
 
 Console.ReadLine();
 
 
 static int MyAtoi(string s)
 {
-    bool positive = true;
+    s = s.TrimStart();
 
-    int i = 0;
-    for (; i < s.Length && s[i] == ' '; i++)
+    if (s.Length == 0)
     {
+        return 0;
     }
 
-    if (i < s.Length)
+    int startIndex = 0;
+    int sign;
+
+    if (s[startIndex] == '-')
     {
-        if (s[i] == '-')
-        {
-            positive = false;
-            i++;
-        }
-        else if (s[i] == '+')
-        {
-            i++;
-        }
+        startIndex++;
+        sign = -1;
+    }
+    else if ((s[startIndex] == '+'))
+    {
+        startIndex++;
+        sign = 1;
+    }
+    else if (Char.IsDigit(s[startIndex]))
+    {
+        sign = 1;
+    }
+    else
+    {
+        return 0;
     }
 
     int val = 0;
-    for (; i < s.Length && char.IsDigit(s[i]); i++)
+
+    for (int i = startIndex; i < s.Length && Char.IsDigit(s[i]); i++)
     {
-        var digit = s[i] - '0';
+        int digit = s[i] - '0';
+
 
         if (val > (int.MaxValue - digit) / 10)
         {
-            return positive ? int.MaxValue : int.MinValue;
+            return (sign == 1) ? int.MaxValue : int.MinValue;
         }
 
         val = val * 10 + digit;
     }
 
-    if (positive)
-    {
-        return val;
-    }
-    else
-    {
-        return val * -1;
-    }
+    return val * sign;
 }
