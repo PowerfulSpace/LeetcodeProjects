39125899cbae1173233a5e159622b3973508b825
Alexander <sany199574@gmail.com>
2024-04-11 19:13:11 +0300

    Добавлен решённый алгоритм

--- project_10_RegularExpressionMatching/Program.cs
+++ project_10_RegularExpressionMatching/Program.cs
@@ -10,9 +10,23 @@ string pattern2 = "a*";
 string input3 = "ab";
 string pattern3 = ".*";
 
-Console.WriteLine(IsMatch(input1,pattern1));
-Console.WriteLine(IsMatch(input2,pattern2));
-Console.WriteLine(IsMatch(input3,pattern3));
+string input4 = "abс";
+string pattern4 = "a***abc";
+
+string input5 = "abcd";
+string pattern5 = "a***abc";
+
+
+string input6 = "abcd++";
+string pattern6 = "a***abc";
+
+
+//Console.WriteLine(IsMatch(input1, pattern1));
+//Console.WriteLine(IsMatch(input2, pattern2));
+//Console.WriteLine(IsMatch(input3, pattern3));
+Console.WriteLine(IsMatch(input4, pattern4));
+Console.WriteLine(IsMatch(input5, pattern5));
+Console.WriteLine(IsMatch(input6, pattern6));
 
 
 Console.ReadLine();
@@ -20,15 +34,45 @@ Console.ReadLine();
 
 static bool IsMatch(string s, string p)
 {
-    Regex regex = new Regex(p);
-    Match result = regex.Match(s);
 
-    if(result.Value == s)
+    char[] quantifiers = new char[] { '+', '*', '?' };
+    List<char> chars = new List<char>();
+    
+    for (int i = 0; i < p.Length -1; i++)
     {
-        return true;
+        if ((p[i] == '+' || p[i] == '*' || p[i] == '?' ))
+        {
+            if(p[i] != p[i + 1])
+            {
+                chars.Add(p[i]);
+            }
+        }
+        else
+        {
+            chars.Add(p[i]);
+        }
     }
-    else
+    chars.Add(p[p.Length-1]);
+
+    p = string.Concat(chars);
+
+    try
+    {
+        Regex regex = new Regex(p);
+        Match result = regex.Match(s);
+
+        if (result.Value == s)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    catch
     {
         return false;
     }
+   
 }
