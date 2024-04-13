8eee4ec423986f80bd96afd0fca312355bf9bda3
Alexander <sany199574@gmail.com>
2024-04-13 16:36:10 +0300

    Добавлен проект по выводу числа в римские цифры

---
+++ project_12_IntegerToRoman/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+
+
+Console.ReadLine();
+
+
+
+static string IntToRoman(int num)
+{
+    return "1";
+}
+
+enum Symbols
+{
+    I = 1,
+    V = 5,
+    X = 10,
+    L = 50,
+    C = 100,
+    D = 500,
+    M = 1000
+}
