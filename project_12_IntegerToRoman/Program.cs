c9dfd302b14aacd353a574c228763b150a96b37e
Alexander <sany199574@gmail.com>
2024-04-13 17:50:32 +0300

    Добавлена логика к алгоритму по трансформации числа в римскую цифру

--- project_12_IntegerToRoman/Program.cs
+++ project_12_IntegerToRoman/Program.cs
@@ -1,7 +1,16 @@
 ﻿
 
+using System.Text;
 
+int input1 = 3;
+int input2 = 58;
+int input3 = 1994;
+int input4 = 55;
 
+//Console.WriteLine(IntToRoman(input1));
+//Console.WriteLine(IntToRoman(input2));
+//Console.WriteLine(IntToRoman(input3));
+Console.WriteLine(IntToRoman(input4));
 
 Console.ReadLine();
 
@@ -9,7 +18,41 @@ Console.ReadLine();
 
 static string IntToRoman(int num)
 {
-    return "1";
+
+    string digits = num.ToString();
+    Stack<string> romanNumerals = new Stack<string>();
+    StringBuilder sb = new StringBuilder();
+
+    int part = (int)char.GetNumericValue(digits[digits.Length - 1]);
+
+    for (int i = digits.Length - 1; i >= 0; i--)
+    {
+        int digit = (int)char.GetNumericValue(digits[i]);
+
+        if (i != digits.Length - 1)
+        {
+            part = digit * 10;
+        }
+
+
+        if(Enum.IsDefined(typeof(Symbols), part))
+        {
+            romanNumerals.Push(Enum.GetName(typeof(Symbols), part));
+            //Console.WriteLine(Enum.GetName(typeof(Symbols), part));
+        }
+        else
+        {
+
+        }
+        
+    }
+
+    while (romanNumerals.Count > 0)
+    {
+        sb.Append(romanNumerals.Pop());
+    }
+
+    return sb.ToString();
 }
 
 enum Symbols
