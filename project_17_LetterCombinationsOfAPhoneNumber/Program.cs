867b90438fa8d0e0fe6a4fc74f3e44e723cc4abf
Alexander <sany199574@gmail.com>
2024-04-18 14:37:16 +0300

    Добавлен Алгоритм

--- project_17_LetterCombinationsOfAPhoneNumber/Program.cs
+++ project_17_LetterCombinationsOfAPhoneNumber/Program.cs
@@ -1,7 +1,10 @@
 ﻿
 
+using System.Text;
+
 string digits = "23";
 
+LetterCombinations(digits);
 
 Console.ReadLine();
 
@@ -9,7 +12,65 @@ Console.ReadLine();
 static IList<string> LetterCombinations(string digits)
 {
 
+    if(!int.TryParse(digits, out int test))
+    {
+        return new List<string>();
+    }
+
+    List<string> result = new List<string>();
+
+    Dictionary<int,List<char>> kit = new Dictionary<int,List<char>>();
+
+    Comparison(kit);
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < digits.Length - 1; i++)
+    {
+        int key = (int)char.GetNumericValue((digits[i]));
+        List<char> letters = kit[key];
+
+        int key2 = (int)char.GetNumericValue((digits[i + 1]));
+        List<char> letters2 = kit[key];
+
+        Console.WriteLine(letters[i] + " " + letters2[i]);
 
 
-    return new List<string>();
+        Console.WriteLine();
+    }
+
+
+
+
+    return result;
+}
+
+static void Comparison(Dictionary<int, List<char>> kit)
+{
+    int index = 97;
+
+    kit.Add(0,new List<char>());
+    kit.Add(1,new List<char>());
+
+    for (int i = 2; i < 10; i++)
+    {
+        List<char> list = new List<char>();
+
+        for (int j = 0; j < 3; j++)
+        {
+            list.Add((char)index);
+            index++;
+        }
+        if(i == 7)
+        {
+            list.Add((char)index);
+            index++;
+        }
+        if(i == 9)
+        {
+            list.Add((char)index);
+            index++;
+        }
+        kit.Add(i,list);
+    }
 }
