26ae78ad2b95357229123b5f2ac31c5c57de7e0d
Alexander <sany199574@gmail.com>
2024-04-21 21:29:08 +0300

    Добавлен алгоритм, решена задача для двусвязанног писка

---
+++ project_19_RemoveNthNodeFromEndOfList/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+
+
+LinkedList<int> input = new LinkedList<int>(new int[] { 1, 2, 3, 4, 5 });
+LinkedList<int> input2 = new LinkedList<int>(new int[] { 1 });
+LinkedList<int> input3 = new LinkedList<int>(new int[] { 1, 2 });
+
+
+Console.WriteLine(RemoveNthFromEnd(input.Last, 2));
+
+
+
+Console.ReadLine();
+
+
+static LinkedListNode<int> RemoveNthFromEnd(LinkedListNode<int> head, int n)
+{
+    if(head.Previous == null && n == 1) { return head; }
+
+    LinkedList<int> result = new LinkedList<int>();
+    Stack<int> save = new Stack<int>();
+
+    int count = 1;
+
+
+    while(head != null)
+    {
+
+        if(count == n)
+        {
+        }
+        else
+        {
+            save.Push(head.Value);       
+        }
+        head = head.Previous;
+        count++;
+    }
+
+    foreach (var item in save)
+    {
+        result.AddLast(item);
+    }
+
+
+    return result.First;
+}
