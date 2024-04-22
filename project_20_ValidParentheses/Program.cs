725d110a4b71dee03f7c6f5a5438b3f627c87438
Alexander <sany199574@gmail.com>
2024-04-22 15:42:32 +0300

    Добавлен алгоритм

--- project_20_ValidParentheses/Program.cs
+++ project_20_ValidParentheses/Program.cs
@@ -3,12 +3,16 @@ string input1 = "()";
 string input2 = "()[]{}";
 string input3 = "(]";
 string input4 = "{[]}";
+string input5 = "({{{{}}}))";
+string input6 = "({))";
 
 
 //Console.WriteLine(IsValid(input1));
 //Console.WriteLine(IsValid(input2));
 //Console.WriteLine(IsValid(input3));
-Console.WriteLine(IsValid(input4));
+//Console.WriteLine(IsValid(input4));
+Console.WriteLine(IsValid(input5));
+Console.WriteLine(IsValid(input6));
 Console.ReadLine();
 
 static bool IsValid(string s)
@@ -20,18 +24,40 @@ static bool IsValid(string s)
         ['{'] = '}'
     };
 
-    for (int i = 0; i < s.Length-1; i++)
+    if(LineTraversal(s, bracket) == "")
     {
+        return true;
+    }
 
+    return false;
+}
+static string LineTraversal(string s, Dictionary<char, char> bracket)
+{
+    if(s == "") { return s; }
+
+    string value = s;
+
+    for (int i = 0; i < Math.Floor((decimal)(s.Length - 1) / 2); i++)
+    {
         if (bracket.ContainsKey(s[i]))
         {
             if (s[i + 1] != bracket[s[i]])
             {
-                return false;
+                value = LineTraversal(s.Substring(1), bracket);
+
+                if (bracket[s[i]].ToString() == value)
+                {
+                    return LineTraversal(value.Substring(1), bracket);
+                }
+            }
+            else
+            {
+                return LineTraversal(value.Substring(2), bracket);
             }
         }
+        else { return value; }
 
     }
 
-    return true;
+    return value;
 }
