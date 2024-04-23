bcd6cc0ab78abbb106799c6dd459a6602358d7e4
Alexander <sany199574@gmail.com>
2024-04-23 03:14:08 +0300

    Добавлен алгоритм

---
+++ project_20_ValidParentheses_6/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+string input1 = "()";
+string input2 = "()[]{}";
+string input3 = "(]";
+string input4 = "{[]}";
+string input5 = "({{{{}}}))";
+string input6 = "({))";
+string input7 = "(}{)";
+string input8 = "([]){";
+
+
+//Console.WriteLine(IsValid(input1));
+//Console.WriteLine(IsValid(input2));
+//Console.WriteLine(IsValid(input3));
+//Console.WriteLine(IsValid(input4));
+Console.WriteLine(IsValid(input5));
+Console.WriteLine(IsValid(input6));
+Console.WriteLine(IsValid(input7));
+Console.WriteLine(IsValid(input8));
+;
+Console.ReadLine();
+
+//Вернуться к этой задаче
+static bool IsValid(string s)
+{
+    HashSet<char> open = new() { '(', '[', '{' };
+    Stack<char> stk = new();
+    foreach (var c in s)
+    {
+        if (open.Contains(c))
+        {
+            stk.Push(c);
+            continue;
+        }
+
+        if (stk.Count() == 0 || Math.Abs(c - stk.Pop()) > 2)
+            return false;
+    }
+
+    return stk.Count() == 0;
+}
---
+++ project_21_MergeTwoSortedLists/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Linq;
+
+LinkedList<int> list1 = new LinkedList<int> (new int[] {1,2,3});
+LinkedList<int> list2 = new LinkedList<int> (new int[] {1,3,4});
+
+Console.WriteLine(MergeTwoLists(list1.First, list2.First));
+
+Console.ReadLine();
+
+
+static LinkedListNode<int> MergeTwoLists(LinkedListNode<int> list1, LinkedListNode<int> list2)
+{
+
+    if(list1 == null) { return list2; }
+    if(list2 == null) { return list1; }
+
+    LinkedList<int> kit1 = new LinkedList<int>(list1.List);
+    LinkedList<int> kit2 = new LinkedList<int>(list2.List);
+
+    List<int> result = new List<int>();
+
+    foreach (var item in kit1)
+        result.Add(item);
+
+    foreach (var item in kit2)
+        result.Add(item);
+
+    result.Sort();
+
+
+    return new LinkedList<int>(result).First;
+}
