9ce7bbf02c63204031d7bddc5ddc606ee479590a
Alexander <sany199574@gmail.com>
2024-04-23 16:59:49 +0300

    Добавлена новая задача

---
+++ project_20_ValidParentheses_7/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+string input1 = "()";
+string input2 = "()[]{}";
+string input3 = "(]";
+string input4 = "{[]}";
+string input5 = "({{{{}}}))";
+string input6 = "({))";
+string input7 = "(}{)";
+string input8 = "([]){";
+
+
+//Console.WriteLine(IsValid(input1));
+Console.WriteLine(IsValid(input2));
+Console.WriteLine(IsValid(input3));
+Console.WriteLine(IsValid(input4));
+Console.WriteLine(IsValid(input5));
+Console.WriteLine(IsValid(input6));
+Console.WriteLine(IsValid(input7));
+Console.WriteLine(IsValid(input8));
+;
+Console.ReadLine();
+
+static bool IsValid(string s)
+{
+    if (s.Length % 2 != 0) return false;
+    Dictionary<char, char> Check = new Dictionary<char, char>();
+    Check['('] = ')';
+    Check['['] = ']';
+    Check['{'] = '}';
+    Stack<char> Stack = new Stack<char>();
+    foreach (char c in s)
+    {
+        if (c == '(' || c == '[' || c == '{')
+        {
+            Stack.Push(c);
+        }
+        else
+        {
+            if (Stack.Count == 0) return false;
+            char top = Stack.Pop();
+            if (Check[top] != c) return false;
+        }
+    }
+    return Stack.Count == 0;
+}
---
+++ project_22_GenerateParentheses/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int num1 = 3;
+int num2 = 1;
+
+Console.WriteLine(GenerateParenthesis(num1));
+//Console.WriteLine(GenerateParenthesis(num2));
+
+
+Console.ReadLine();
+
+
+static IList<string> GenerateParenthesis(int n)
+{
+
+
+
+
+
+
+    return new List<string>();
+}
