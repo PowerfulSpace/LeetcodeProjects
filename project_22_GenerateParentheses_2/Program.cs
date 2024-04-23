10e858257b2c9315c5642f752db1ea0c0c6e10b0
Alexander <sany199574@gmail.com>
2024-04-24 00:05:39 +0300

    Исправлен алгоритм

---
+++ project_22_GenerateParentheses_2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Text;
+
+int num1 = 3;
+int num2 = 1;
+
+//Console.WriteLine(GenerateParenthesis(num1));
+Console.WriteLine(GenerateParenthesis(num2));
+
+
+Console.ReadLine();
+
+
+static IList<string> GenerateParenthesis(int n)
+{
+    if (n < 1) return new List<string>();
+
+    char left = '(';
+    char rigth = ')';
+
+    List<string> output = new List<string>();
+
+    StringBuilder sb = new StringBuilder();
+
+   
+
+
+    return output;
+}
+
