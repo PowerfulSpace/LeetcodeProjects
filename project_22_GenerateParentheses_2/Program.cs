711a2c691bfd3aef74f841a71cf45bb388664ef8
Alexander <sany199574@gmail.com>
2024-04-25 19:13:29 +0300

    Добавлен алгоритм

--- project_22_GenerateParentheses_2/Program.cs
+++ project_22_GenerateParentheses_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System.Text;
+IList<string> res = new List<string>();
 
 int num1 = 3;
 int num2 = 1;
@@ -14,98 +15,46 @@ Console.WriteLine(GenerateParenthesis(num3));
 Console.ReadLine();
 
 
-static IList<string> GenerateParenthesis(int n)
+IList<string> GenerateParenthesis(int n)
 {
-    if (n < 1) return new List<string>();
-
-    char left = '(';
-    char rigth = ')';
-
-    List<string> output = new List<string>();
-
-    StringBuilder sb = new StringBuilder();
-
-
-    for (int i = 0; i < n; i++)
-    {
-        sb.Append('(');
-        sb.Append(')');
-    }
-    string startValue = sb.ToString();
-    string combination1tValue = string.Empty;
-
-
-    AddCombination(sb.ToString(), output);
-
-    int leftIndex = 1;
-    int rigthIndex = 2;
-
-    for (int i = 1; i < n; i++)
-    {       
-       leftIndex = i;
-       rigthIndex = i + 1;
-
-        while (rigthIndex + 1 < sb.Length)
-        {
-            left = sb[leftIndex];
-            rigth = sb[rigthIndex];
-
-            if (left != rigth)
-            {
-                sb.Remove(leftIndex, 2);
-
-                sb.Insert(i, left);
-                sb.Insert(i, rigth);
-
-                AddCombination(sb.ToString(), output);
-            }          
-
-            leftIndex = rigthIndex;
-            rigthIndex = rigthIndex + 1;
-
-        }
-        if(i == 1)
-        {
-            combination1tValue = sb.ToString();
-        }
-    }
-
-    sb.Clear();
-    sb.Append(combination1tValue);
-
-    leftIndex = 1;
-    rigthIndex = 2;
-
-    int index = 1;
-
-    while (rigthIndex + 1 < sb.Length)
-    {
-        left = sb[leftIndex];
-        rigth = sb[rigthIndex];
-
-        if (left != rigth)
-        {
-            sb.Remove(leftIndex, 2);
-
-            sb.Insert(index, left);
-            sb.Insert(index, rigth);
-
-            AddCombination(sb.ToString(), output);
-        }
-        index++;
-        leftIndex = rigthIndex;
-        rigthIndex = rigthIndex + 1;
-
-    }
-
-    return output;
+    StringBuilder sb = new();
+    Backtrack(n, n, sb);
+    return res;
 }
 
 
-static void AddCombination(string combination, List<string> list)
+void Backtrack(int open, int closed, StringBuilder sb)
 {
-    if (!list.Contains(combination))
+    if (open > closed)
+        return;
+
+    if (open == 0 && closed == 0)
     {
-        list.Add(combination);
+        string str = sb.ToString();
+        res.Add(str);
+        return;
     }
+
+    if (open == closed)
+    {
+        sb.Append("(");
+        Backtrack(open - 1, closed, sb);
+        sb.Length--;
+        return;
+    }
+
+    if (open < closed)
+    {
+        if (open > 0)
+        {
+            sb.Append("(");
+            Backtrack(open - 1, closed, sb);
+            sb.Length--;
+        }
+
+        sb.Append(")");
+        Backtrack(open, closed - 1, sb);
+        sb.Length--;
+    }
+
 }
