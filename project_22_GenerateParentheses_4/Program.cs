c171c42cb5fa57e05f3ae2fedbb541f0a73714bc
Alexander <sany199574@gmail.com>
2024-04-28 10:26:07 +0300

    Добавлено новое решение для алгоритма

---
+++ project_22_GenerateParentheses_4/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+int num1 = 1;
+int num2 = 2;
+int num3 = 3;
+int num4 = 4;
+
+//Console.WriteLine(GenerateParenthesis(num1));
+//Console.WriteLine(GenerateParenthesis(num2));
+//Console.WriteLine(GenerateParenthesis(num3));
+Console.WriteLine(GenerateParenthesis(num4));
+
+
+Console.ReadLine();
+
+
+IList<string> GenerateParenthesis(int n)
+{
+    List<string> result = new List<string>();
+
+    Gen(result, "", n, 0);
+
+    return result;
+}
+
+
+static void Gen(IList<string> all, string pattern, int toOpen, int opened)
+{
+
+    if (opened < 0)
+    {
+        return;
+    }
+
+    if (toOpen == 0)
+    {
+        if (opened > 0)
+        {
+            pattern += new string(')', opened);
+            opened = 0;
+        }
+    }
+    else
+    {
+        Gen(all, pattern + "(", toOpen - 1, opened + 1);
+        Gen(all, pattern + "(", toOpen, opened - 1);
+    }
+
+    if (toOpen == 0 && opened == 0)
+    {
+        all.Add(pattern);
+    }
+
+}
+
+
+
