72b6d830a4634038c3b311d50505a9f2b447befd
Alexander <sany199574@gmail.com>
2024-04-29 08:15:13 +0300

    Добавлен проект

--- project_02_AddTwoNumbers_5/Program.cs
+++ project_02_AddTwoNumbers_5/Program.cs
@@ -26,10 +26,13 @@ static ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry = 0)
 
 
 
-public class ListNode {
+
+public class ListNode
+{
     public int val;
     public ListNode next;
-    public ListNode(int val=0, ListNode next=null) {
+    public ListNode(int val = 0, ListNode next = null)
+    {
         this.val = val;
         this.next = next;
     }
