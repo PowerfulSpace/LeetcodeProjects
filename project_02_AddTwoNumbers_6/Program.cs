2bffb98d1811f222b9deba1080db9dcb94b52ef9
Alexander <sany199574@gmail.com>
2024-04-29 08:23:35 +0300

    Добавлен проект

--- project_02_AddTwoNumbers_6/Program.cs
+++ project_02_AddTwoNumbers_6/Program.cs
@@ -45,10 +45,12 @@ static int GetRemainder(ListNode listNode)
     return response;
 }
 
-public class ListNode {
+public class ListNode
+{
     public int val;
     public ListNode next;
-    public ListNode(int val=0, ListNode next=null) {
+    public ListNode(int val = 0, ListNode next = null)
+    {
         this.val = val;
         this.next = next;
     }
