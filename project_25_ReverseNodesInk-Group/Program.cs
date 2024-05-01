6d1686f57db5870dfd396e4dcb46fa5b29d154b1
Alexander <sany199574@gmail.com>
2024-05-01 12:57:48 +0300

    Добавлен проект

---
+++ project_25_ReverseNodesInk-Group/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+ReverseKGroup(l1, 2);
+
+Console.ReadLine();
+
+
+static ListNode ReverseKGroup(ListNode head, int k)
+{
+
+
+
+    return head;
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
