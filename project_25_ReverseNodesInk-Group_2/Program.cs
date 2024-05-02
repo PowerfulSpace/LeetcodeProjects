77e3893f670099acfaf0e349dd7b45403cdba0e5
Alexander <sany199574@gmail.com>
2024-05-02 13:55:48 +0300

    Добавлен алгоритм

--- project_25_ReverseNodesInk-Group_2/Program.cs
+++ project_25_ReverseNodesInk-Group_2/Program.cs
@@ -7,13 +7,36 @@ Console.ReadLine();
 
 static ListNode ReverseKGroup(ListNode head, int k)
 {
-  
-
-
+    ListNode curr = head;
+    int count = 0;
+    while (curr != null && count < k)
+    {
+        curr = curr.next;
+        count++;
+    }
+    if (count == k)
+    {
+        ListNode newHead = ReverseKGroup(curr, k);
+        var result = Reverse(head, k, newHead);
+        return result;
+    }
     return head;
 }
 
+static ListNode Reverse(ListNode head, int k, ListNode newHead)
+{
+    ListNode curr = head;
+    ListNode prev = newHead;
 
+    for (int i = 0; i < k; i++)
+    {
+        ListNode nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+}
 
 
 
