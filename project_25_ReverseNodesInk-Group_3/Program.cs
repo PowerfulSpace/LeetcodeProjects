3afcfa2699ce53d8d90a134801a046b74743c1b9
Alexander <sany199574@gmail.com>
2024-05-02 13:57:30 +0300

    Дописан базовый код

--- project_25_ReverseNodesInk-Group_3/Program.cs
+++ project_25_ReverseNodesInk-Group_3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+ReverseKGroup(l1, 3);
+
+Console.ReadLine();
+
+
+static ListNode ReverseKGroup(ListNode head, int k)
+{
+   
+    return head;
+}
+
+
+
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
