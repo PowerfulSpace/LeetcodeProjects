f3677ba435a2a8dfd8cad818c9f4afd4eeea61d1
Alexander <sany199574@gmail.com>
2024-05-03 14:39:07 +0300

    Добавлен алгоритм

---
+++ project_26_RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+int[] nums1 = { 1, 1, 2 };
+int[] nums2 = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+Console.WriteLine(RemoveDuplicates(nums1));
+Console.WriteLine(RemoveDuplicates(nums2));
+
+
+Console.ReadLine();
+
+
+static int RemoveDuplicates(int[] nums)
+{
+    if (nums.Length < 1) { return 0; }
+
+    int[] array = nums.ToList().Distinct().ToArray();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        nums[i] = array[i];
+
+    }
+
+    return array.Length;
+}
