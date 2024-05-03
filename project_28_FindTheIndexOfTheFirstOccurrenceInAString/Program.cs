eb4b6ab9c181c0e182f5eae3261f2579d0f3e6d2
Alexander <sany199574@gmail.com>
2024-05-03 21:06:26 +0300

    Добавлен алгоритм

--- project_28_FindTheIndexOfTheFirstOccurrenceInAString/Program.cs
+++ project_28_FindTheIndexOfTheFirstOccurrenceInAString/Program.cs
@@ -1,5 +1,20 @@
 ﻿
 
 
+string haystack1 = "sadbutsad";
+string needle1= "sad";
+
+string haystack2 = "leetcode";
+string needle2 = "leeto";
+
+//Console.WriteLine(StrStr(haystack1, needle1));
+Console.WriteLine(StrStr(haystack2, needle2));
 
 Console.ReadLine();
+
+
+
+static int StrStr(string haystack, string needle)
+{
+    return haystack.IndexOf(needle);
+}
