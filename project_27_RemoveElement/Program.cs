705c5bde968d9553f6caa94a6fe0f6a9044f1a28
Alexander <sany199574@gmail.com>
2024-05-03 20:29:50 +0300

    Добавлен алгоритм

---
+++ project_27_RemoveElement/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int[] nums1 = { 3, 2, 2, 3 };
+int[] nums2 = { 0, 1, 2, 2, 3, 0, 4, 2 };
+
+Console.WriteLine(RemoveElement(nums1, 3));
+Console.WriteLine(RemoveElement(nums2,2));
+
+
+
+
+Console.ReadLine();
+
+
+
+static int RemoveElement(int[] nums, int val)
+{
+    int index = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if(nums[i] == val)
+        {
+            continue;
+        }
+        nums[index] = nums[i];
+        index++;
+
+    }
+    return index;
+}
