bc8ba4472150e811d652f119d5bf2c2f0d101dd7
Alexander <sany199574@gmail.com>
2024-05-04 13:55:06 +0300

    Добавлен алгоритм

--- project_29_DivideTwoIntegers/Program.cs
+++ project_29_DivideTwoIntegers/Program.cs
@@ -6,13 +6,36 @@ int divisor1 = 3;
 int divident2 = 7;
 int divisor2 = -3;
 
-Console.WriteLine(Divide(divident1, divisor1));
-Console.WriteLine(Divide(divident2, divisor2));
+int divident3 = -2147483648;
+int divisor3 = -1;
+
+//Console.WriteLine(Divide(divident1, divisor1));
+//Console.WriteLine(Divide(divident2, divisor2));
+Console.WriteLine(Divide(divident3, divisor3));
 
 Console.ReadLine();
 
 
 static int Divide(int dividend, int divisor)
 {
-    return 1;
+
+    if (divisor == 0 && dividend < int.MaxValue && dividend > int.MinValue) { return dividend; }
+
+    if (dividend >= int.MaxValue) { return int.MaxValue / divisor; }
+    if(dividend <= int.MinValue)
+    {
+        if(divisor < 0)
+        {
+            if(divisor == -1)
+            {
+                return int.MaxValue;
+            }
+        }
+        else
+        {
+            return int.MinValue / divisor;
+        }
+    }
+
+    return dividend / divisor;
 }
