f68ebe62eba2590d1256a2700d83825fd11ab60b
Alexander <sany199574@gmail.com>
2024-05-05 20:28:31 +0300

    Добавлен алгоритм

--- project_30_SubstringWithConcatenationOfAllWords/Program.cs
+++ project_30_SubstringWithConcatenationOfAllWords/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Text;
+
 string str1 = "barfoothefoobarman";
 string[] words1 = { "foo", "bar" };
 
@@ -8,24 +11,57 @@ string[] words2 = { "word", "good", "best", "word" };
 string str3 = "barfoofoobarthefoobarman";
 string[] words3 = { "bar", "foo", "the" };
 
-FindSubstring(str1, words1);
+string str4 = "";
+string[] words4 = { };
+
+//FindSubstring(str1, words1);
 FindSubstring(str2, words2);
-FindSubstring(str3, words3);
+//FindSubstring(str3, words3);
+//FindSubstring(str4, words4);
 
 
 Console.ReadLine();
 
 static IList<int> FindSubstring(string s, string[] words)
 {
-	List<int> indexs = new List<int>();
+	if(words.Length < 1 || words[0].Length > s.Length) { return new List<int>(); }
 
-	for (int i = 0; i < words.Length; i++)
-	{
-		if (!s.Contains(words[i])) { return new List<int>(); }
+    StringBuilder str = new StringBuilder(s);
+    int wordLength = words[0].Length;
+    int index = 0;
 
-		indexs.Add(s.IndexOf(words[i]));
+    List<int> blockIndexes = new List<int>();
+
+    string checkString = s;
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (blockIndexes.Contains(index))
+        {
+            checkString = checkString.Substring(index + wordLength);
+        }
+
+        if (!checkString.Contains(words[i])) { return new List<int>(); }
+
+        index = checkString.IndexOf(words[i]);
+
+        blockIndexes.Add(index);
+        str.Remove(index, wordLength);
+        str.Insert(index, "_", wordLength);
+    }
+
+    str.Clear();
+    str.Append(s);
+
+    List<int> indexs = new List<int>();
+
+
+    for (int i = 0; i < s.Length; i++)
+    {
 
     }
+
+
 
     return indexs;
 }
