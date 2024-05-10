800098c98c80d04d46170590a77428a739b75d15
Alexander <sany199574@gmail.com>
2024-05-11 02:08:47 +0300

    Добавлен проект

---
+++ project_31_Next Permutation/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+int[] nums1 = { 1, 2, 3 };
+int[] nums2 = { 3, 2, 1 };
+int[] nums3 = { 1, 1, 5 };
+
+
+NextPermutation(nums1);
+NextPermutation(nums2);
+NextPermutation(nums3);
+
+
+Console.ReadLine();
+
+static void NextPermutation(int[] nums)
+{
+
+}
