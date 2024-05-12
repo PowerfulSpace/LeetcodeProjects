c749e1492ceff7e5fbe7433145ec1110467bb406
Alexander <sany199574@gmail.com>
2024-05-12 08:27:44 +0300

    Добавлен алгоритм

---
+++ project_31_Next Permutation_2/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+int[] nums1 = { 1, 2, 3 };
+int[] nums2 = { 3, 2, 1 };
+int[] nums3 = { 1, 1, 5 };
+int[] nums4 = { 1, 3, 2 };
+int[] nums5 = { 2, 3, 1 };
+int[] nums6 = { 5, 4, 7, 5, 3, 2 };
+int[] nums7 = { 2, 2, 7, 5, 4, 3, 2, 2, 1 };
+int[] nums8 = { 1, 4, 3, 2, 2 };
+
+Print(nums1);
+Print(nums2);
+Print(nums3);
+Print(nums4);
+Print(nums5);
+Print(nums6);
+Print(nums7);
+Print(nums8);
+
+Console.ReadLine();
+
+
+static void Print(int[] nums)
+{
+    Array.ForEach(nums, x => Console.Write(x + " "));
+    NextPermutation(nums);
+    Console.Write(" - - - ");
+    Array.ForEach(nums, x => Console.Write(x + " "));
+    Console.WriteLine();
+}
+
+static void NextPermutation(int[] nums)
+{
+    int index = -1;
+    int n = nums.Length;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (nums[i] < nums[i + 1])
+        {
+            index = i;
+            break;
+        }
+    }
+    if (index == -1)
+    {
+        Array.Reverse(nums, 0, n);
+        return;
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (nums[index] < nums[i])
+        {
+            int swap = 0;
+            swap = nums[i];
+            nums[i] = nums[index];
+            nums[index] = swap;
+            break;
+        }
+    }
+    Array.Reverse(nums, index + 1, n - 1 - index);
+}
