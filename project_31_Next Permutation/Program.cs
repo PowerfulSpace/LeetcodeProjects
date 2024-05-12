1ce9be0825d738562031f96210cb29a2c1f48a94
Alexander <sany199574@gmail.com>
2024-05-12 07:49:26 +0300

    Добавлен алгоритм

--- project_31_Next Permutation/Program.cs
+++ project_31_Next Permutation/Program.cs
@@ -6,13 +6,17 @@ int[] nums3 = { 1, 1, 5 };
 int[] nums4 = { 1, 3, 2 };
 int[] nums5 = { 2, 3, 1 };
 int[] nums6 = { 5, 4, 7, 5, 3, 2 };
+int[] nums7 = { 2, 2, 7, 5, 4, 3, 2, 2, 1 };
+int[] nums8 = { 5, 4, 7, 5, 3, 2 };
 
-Print(nums1);
-Print(nums2);
-Print(nums3);
-Print(nums4);
-Print(nums5);
-Print(nums6);
+//Print(nums1);
+//Print(nums2);
+//Print(nums3);
+//Print(nums4);
+//Print(nums5);
+//Print(nums6);
+//Print(nums7);
+Print(nums8);
 
 Console.ReadLine();
 
@@ -32,25 +36,65 @@ static void NextPermutation(int[] nums)
 
 	bool isSortDesc = false;
 
-	for (int i = nums.Length - 1; i > 0; i--)
+	for (int i = nums.Length -1; i > 0; i--)
 	{
+        if(i != nums.Length - 1 && i - 1 != 0)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                List<int> newArray = new List<int>();
+                
+                int next = nums[i];
+                for (int j = i; j < nums.Length - 1; j++)
+                {
+                    if(nums[j] >= nums[i - 2] && (next > nums[j]))
+                    {
+                        next = nums[j];
+                    }
+                }
+                newArray.Add(next);
+
+
+                List<int> array = nums.Skip(i - 1).Take(nums.Length - (i - 1)).ToList();
+                array.Remove(newArray.First());
+                array.Sort();
+                newArray.AddRange(array);
+
+                int index = i - 1;
+                for (int j = 0; j < newArray.Count; j++)
+                {
+                    nums[index] = newArray[j];
+                    index++;
+                }
+                break;
+            }
+        }
+
+        if (i != nums.Length - 1 && nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
+        {
+            if (nums[i - 1] < nums[i + 1])
+            {
+                int swap = nums[i - 1];
+                nums[i - 1] = nums[i + 1];
+                nums[i + 1] = nums[i];
+                nums[i] = swap;
+                break;
+            }
+            if (nums[i - 1] > nums[i + 1])
+            {
+                int swap = nums[i - 1];
+                nums[i - 1] = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = swap;
+                break;
+            }
+        }
 
         if (nums[i] > nums[i - 1])
         {
-            List<int> newArray = new List<int>();
-            newArray.Add(nums[i + 1]);
-
-            List<int> array = nums.Skip(i - 1).Take(nums.Length - (i - 1)).ToList();
-            array.Remove(newArray[0]);
-            array.Sort();
-            newArray.AddRange(array);
-
-            int index = i - 1;
-            for (int j = 0; j < newArray.Count; j++)
-            {
-                nums[index] = newArray[j];
-                index++;
-            }
+            int swap = nums[i];
+            nums[i] = nums[i - 1];
+            nums[i - 1] = swap;
             break;
         }
 
