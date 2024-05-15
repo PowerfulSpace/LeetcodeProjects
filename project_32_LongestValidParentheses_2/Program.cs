fb6185f10b44a648f3943f6a291c19f0db2af005
Alexander <sany199574@gmail.com>
2024-05-15 19:38:15 +0300

    Дописан алгоритм

--- project_32_LongestValidParentheses_2/Program.cs
+++ project_32_LongestValidParentheses_2/Program.cs
@@ -7,10 +7,10 @@ string input4 = "()(())";
 string input5 = "()(()";
 
 
-//Console.WriteLine(LongestValidParentheses(input1));
-//Console.WriteLine(LongestValidParentheses(input2));
-//Console.WriteLine(LongestValidParentheses(input3));
-//Console.WriteLine(LongestValidParentheses(input4));
+Console.WriteLine(LongestValidParentheses(input1));
+Console.WriteLine(LongestValidParentheses(input2));
+Console.WriteLine(LongestValidParentheses(input3));
+Console.WriteLine(LongestValidParentheses(input4));
 Console.WriteLine(LongestValidParentheses(input5));
 
 Console.ReadLine();
