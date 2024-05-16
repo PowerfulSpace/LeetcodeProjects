b9e48bd54db44c8a1de424f689a92d6b538bcb37
Alexander <sany199574@gmail.com>
2024-05-16 12:58:48 +0300

    Добавлен алгоритм

--- project_32_LongestValidParentheses_4/Program.cs
+++ project_32_LongestValidParentheses_4/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Diagnostics.Metrics;
+
 string input1 = "(()";
 string input2 = ")()())";
 string input3 = "";
@@ -18,4 +20,43 @@ Console.ReadLine();
 static int LongestValidParentheses(string s)
 {
  
+    Stack<int> indexes = new Stack<int>();
+    bool[] coincidences = new bool[s.Length];
+
+	for (int i = 0; i < s.Length; i++)
+	{
+        if (s[i] == '(')
+        {
+            indexes.Push(i);
+        }
+        else
+        {
+            if(indexes.Count > 0)
+            {
+                coincidences[indexes.Pop()] = true;
+                coincidences[i] = true;
+            }
+        }
+    }
+
+    int combinations = 0;
+    int count = 0;
+    foreach (var coincidence in coincidences)
+    {
+        if (coincidence)
+        {
+            count++;
+            if(combinations < count)
+            {
+                combinations = count;
+            }
+        }
+        else
+        {
+            count = 0;
+        }
+    }
+
+
+    return combinations;
 }
