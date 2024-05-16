adb15dc398880a8368dc4e47fde0a628467befc8
Alexander <sany199574@gmail.com>
2024-05-16 13:00:36 +0300

    Добавлен алгоритм

--- project_32_LongestValidParentheses_4/Program.cs
+++ project_32_LongestValidParentheses_4/Program.cs
@@ -1,8 +1,4 @@
-﻿
-
-using System.Diagnostics.Metrics;
-
-string input1 = "(()";
+﻿string input1 = "(()";
 string input2 = ")()())";
 string input3 = "";
 string input4 = "()(())";
@@ -19,44 +15,30 @@ Console.ReadLine();
 
 static int LongestValidParentheses(string s)
 {
- 
-    Stack<int> indexes = new Stack<int>();
-    bool[] coincidences = new bool[s.Length];
 
-	for (int i = 0; i < s.Length; i++)
-	{
+    Stack<int> stack = new Stack<int>();
+    stack.Push(-1);
+    int maxLength = 0;
+
+    for (int i = 0; i < s.Length; i++)
+    {
         if (s[i] == '(')
         {
-            indexes.Push(i);
+            stack.Push(i);
         }
         else
         {
-            if(indexes.Count > 0)
+            stack.Pop();
+
+            if (stack.Count == 0)
             {
-                coincidences[indexes.Pop()] = true;
-                coincidences[i] = true;
+                stack.Push(i);
+            }
+            else
+            {
+                maxLength = Math.Max(maxLength, i - stack.Peek());
             }
         }
     }
-
-    int combinations = 0;
-    int count = 0;
-    foreach (var coincidence in coincidences)
-    {
-        if (coincidence)
-        {
-            count++;
-            if(combinations < count)
-            {
-                combinations = count;
-            }
-        }
-        else
-        {
-            count = 0;
-        }
-    }
-
-
-    return combinations;
+    return maxLength;
 }
