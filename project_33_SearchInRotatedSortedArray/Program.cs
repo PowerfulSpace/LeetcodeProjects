c2973cfc693c68dd07c8ca05fc8eaed0ecb3b00c
Alexander <sany199574@gmail.com>
2024-05-17 12:37:49 +0300

    Добавлен проект

---
+++ project_33_SearchInRotatedSortedArray/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int[] nums1 = { 4, 5, 6, 7, 0, 1, 2 };
+int[] nums2 = { 1};
+
+Console.WriteLine(Search(nums1,0));
+Console.WriteLine(Search(nums1,3));
+Console.WriteLine(Search(nums2,0));
+
+Console.ReadLine();
+
+
+
+static int Search(int[] nums, int target)
+{
+
+
+    return 1;
+}
+
+
