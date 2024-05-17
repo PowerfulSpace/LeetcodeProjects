1036a6de1168ff914e1d66a5a2b42458e9d89259
Alexander <sany199574@gmail.com>
2024-05-17 13:34:50 +0300

    Добавлена реализация

---
+++ project_35_SearchInsertPosition/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int[] nums1 = { 1, 3, 5, 6 };
+
+Console.WriteLine(SearchInsert(nums1, 5));
+Console.WriteLine(SearchInsert(nums1, 2));
+Console.WriteLine(SearchInsert(nums1, 7));
+
+Console.ReadLine();
+
+
+
+static int SearchInsert(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if(nums[i] >= target) { return i; }
+    }
+
+    return nums.Length;
+}
+
+
