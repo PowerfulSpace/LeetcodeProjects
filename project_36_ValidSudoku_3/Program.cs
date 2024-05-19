d4dff0662327eac26f7e5ba4178b0daba83b4e5c
Alexander <sany199574@gmail.com>
2024-05-19 21:44:08 +0300

    Добавлен алгоритм

---
+++ project_36_ValidSudoku_3/Program.cs
@@ -0,0 +1,58 @@
+﻿
+char[][] board1 = {
+    new char[]{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+    new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+    new char[]{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+    new char[]{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+    new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+    new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+    new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+    new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+    new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+
+char[][] board2 = {
+    new char[]{'8','3','.','.','7','.','.','.','.'},
+    new char[]{'6','.','.','1','9','5','.','.','.'},
+    new char[]{'.','9','8','.','.','.','.','6','.'},
+    new char[]{'8','.','.','.','6','.','.','.','3'},
+    new char[]{'4','.','.','8','.','3','.','.','1'},
+    new char[]{'7','.','.','.','2','.','.','.','6'},
+    new char[]{'.','6','.','.','.','.','2','8','.'},
+    new char[]{'.','.','.','4','1','9','.','.','5'},
+    new char[]{'.','.','.','.','8','.','.','7','9'} };
+
+
+
+
+Console.WriteLine(IsValidSudoku(board1));
+Console.WriteLine(IsValidSudoku(board2));
+
+
+Console.ReadLine();
+
+static bool IsValidSudoku(char[][] board)
+{
+
+    HashSet<char>[] x = new HashSet<char>[9];
+    HashSet<char>[] y = new HashSet<char>[9];
+    HashSet<char>[] chanks = new HashSet<char>[9];
+
+    for (int i = 0; i < 9; i++)
+    {
+        x[i] = new HashSet<char>();
+        y[i] = new HashSet<char>();
+        chanks[i] = new HashSet<char>();
+    }
+
+    for (int i = 0; i < board.Length; i++)
+    {
+        for (int j = 0; j < board[i].Length; j++)
+        {
+            Console.Write(board[i][j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    return default;
+}
