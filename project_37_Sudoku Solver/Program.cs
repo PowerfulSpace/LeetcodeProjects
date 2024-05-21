50acf1c1bf9405a8d6f9d6724a128d9c294ae4da
Alexander <sany199574@gmail.com>
2024-05-22 00:04:14 +0300

    Добавлена проверка

--- project_37_Sudoku Solver/Program.cs
+++ project_37_Sudoku Solver/Program.cs
@@ -30,14 +30,77 @@ Console.ReadLine();
 
 static void SolveSudoku(char[][] board)
 {
+    List<char>[] rows = new List<char>[9];
+    List<char>[] cols = new List<char>[9];
+    List<char>[] chancks = new List<char>[9];
 
+    
+    bool isValid = IsValidSudoku(board, rows, cols, chancks);
+    if(!isValid) { return; }
+
+    Dictionary<char, int> priorityCheck = ScanPriorityCheck(rows);
+
+    Console.WriteLine();
 }
 
 
-static bool IsValidSudoku(char[][] board)
+static bool IsValidSudoku(char[][] board, List<char>[] rows, List<char>[] cols, List<char>[] chancks)
 {
-    
+    for (int i = 0; i < 9; i++)
+    {
+        rows[i] = new List<char>();
+        cols[i] = new List<char>();
+        chancks[i] = new List<char>();
+    }
 
+    for (int row = 0; row < board.Length; row++)
+    {
+        for (int col = 0; col < board[row].Length; col++)
+        {
+            char item = board[row][col];
+
+            int chanck = ((row / 3) * 3) + (col / 3);
+            if (item != '.')
+            {
+                if (rows[row].Contains(item) || cols[col].Contains(item) || chancks[chanck].Contains(item)) { return false; }
+            }
+
+            rows[row].Add(item);
+            cols[col].Add(item);
+            chancks[chanck].Add(item);
+        }
+    }
 
     return true;
 }
+
+static Dictionary<char, int> ScanPriorityCheck(List<char>[] rows)
+{
+    Dictionary<char, int> priorityCheck = new Dictionary<char, int>();
+
+    for (int i = 0; i < rows.Length; i++)
+    {
+        for (int j = 0; j < rows.Length; j++)
+        {
+            if (rows[i][j] != '.')
+            {
+                if (priorityCheck.ContainsKey(rows[i][j]))
+                {
+                    if (priorityCheck[rows[i][j]] == 8)
+                    {
+
+                    }
+                    else
+                    {
+                        priorityCheck[rows[i][j]]++;
+                    }         
+                }
+                else { priorityCheck.Add(rows[i][j], 0); }
+                
+            }
+        }
+    }
+
+    return priorityCheck;
+}
+
