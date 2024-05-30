124f9b119411f72c6f41a096017e32e0becafebb
Alexander <sany199574@gmail.com>
2024-05-30 10:13:31 +0300

    Удалён алгоритм

--- project_37_Sudoku Solver_3/Program.cs
+++ project_37_Sudoku Solver_3/Program.cs
@@ -33,104 +33,12 @@ HashSet<char>[] chunks;
 
 void SolveSudoku(char[][] board)
 {
-    rows = new HashSet<char>[board.Length];
-    cols = new HashSet<char>[board.Length];
-    chunks = new HashSet<char>[board.Length];
-
-    for (int row = 0; row < board.Length; row++)
-    {
-        for (int col = 0; col < board[row].Length; col++)
-        {
-            rows[col] = new HashSet<char>();
-            cols[col] = new HashSet<char>();
-            chunks[col] = new HashSet<char>();
-        }
-    }
-
-    for (int row = 0; row < board.Length; row++)
-    {
-        for (int col = 0; col < board[row].Length; col++)
-        {
-            char item = board[row][col];
-            if(item == '.') { continue; }
-
-            int chunck = ((row / 3) * 3) + (col / 3);
-
-            rows[row].Add(item);
-            cols[col].Add(item);
-            chunks[chunck].Add(item);
-
-        }
-    }
-
-    Selection(board, 0, 0);
+   
 }
 
 
 
-bool Selection(char[][] board, int row, int col)
-{
-    if (row == 9 || row == 8 && col == 8)
-    {
-        return true;
-    }
 
-    char item = board[row][col];
-
-    while (item != '.')
-    {
-        col++;
-        if (col == 9)
-        {
-            row++;
-            col = 0;
-        }
-
-        if (row == 9 || row == 8 && col > 8)
-        {
-            return true;
-        }
-        item = board[row][col];
-    }
-
-    bool isValid = false;
-    int chunk = ((row / 3) * 3) + (col / 3);
-
-    for (int i = 1; i <= 9; i++)
-    {
-        char currentItem = Convert.ToChar(i + 48);
-
-       
-
-        if (rows[row].Contains(currentItem) || cols[col].Contains(currentItem) || chunks[chunk].Contains(currentItem))
-        {
-            continue;
-        }
-
-        board[row][col] = currentItem;
-        rows[row].Add(currentItem);
-        cols[col].Add(currentItem);
-        chunks[chunk].Add(currentItem);
-
-        isValid = Selection(board, row, col);
-
-        if (isValid)
-        {
-            return true;
-        }
-        else
-        {
-            board[row][col] = '.';
-            rows[row].Remove(currentItem);
-            cols[col].Remove(currentItem);
-            chunks[chunk].Remove(currentItem);
-        }
-
-    }
-
-
-    return isValid;
-}
 
 static void Print(char[][] array)
 {
