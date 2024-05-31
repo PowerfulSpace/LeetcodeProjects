2d360750c080b23154b13fbdad2b909fc7a6b536
Alexander <sany199574@gmail.com>
2024-05-31 17:20:04 +0300

    Добавлен алгоритм

---
+++ project_39_CombinationSum/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+int[] candidates1 = { 2, 3, 6, 7 };
+int target1 = 7;
+
+CombinationSum(candidates1, target1);
+
+Console.ReadLine();
+
+IList<IList<int>> CombinationSum(int[] candidates, int target)
+{
+
+    List<IList<int>> result = new List<IList<int>>();
+
+
+
+
+    return result;
+}
+
+int GetCombination()
+{
+    return 1;
+}
