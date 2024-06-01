19e4f3ffcef561696f7b6f574bad5a7ebf0118de
Alexander <sany199574@gmail.com>
2024-06-01 16:36:35 +0300

    переписал алгоритм

--- project_39_CombinationSum/Program.cs
+++ project_39_CombinationSum/Program.cs
@@ -9,42 +9,46 @@ Console.ReadLine();
 
 IList<IList<int>> CombinationSum(int[] candidates, int target)
 {
-    List<IList<int>> result = GetCombination(candidates, new List<IList<int>>(), target, 0,0);
+    List<IList<int>> result = GetCombination(candidates, new List<IList<int>>(), target, 0, 0, new List<int>());
 
     return result;
 }
 
-List<IList<int>> GetCombination(int[] array,List<IList<int>> result, int target, int index,int sum)
+List<IList<int>> GetCombination(int[] array, List<IList<int>> result, int target, int index, int sum, List<int> list)
 {
-    if (index == array.Length) { return result; }
-
-    List<int> kit =new List<int>();
-    int curIndex = 0;
-
-    while(sum < target)
+    if (index == 3)
     {
-        sum += array[curIndex];
+        Console.WriteLine();
+    }
+    if (index == array.Length || sum > target) { return result; }
 
-        if(sum < target)
+    List<int> kit = new List<int>(list);
+
+    while (sum < target)
+    {
+        kit.Add(array[index]);
+        sum += array[index];
+
+        if (sum == 11)
         {
-            GetCombination(array, result, target, index, sum);
+            Console.WriteLine();
+        }
 
-            if(sum > target) { return result; }
+        GetCombination(array, result, target, index, sum, kit.Take(kit.Count).ToList());
 
-            kit.Add(array[curIndex]);
+        if (sum > target)
+        {
+            kit.Remove(array[index]);
         }
 
         if (sum == target)
         {
-            result.Add(kit);
+            result.Add(kit.Take(kit.Count).ToList());
+            kit.Clear();
             return result;
         }
-        curIndex++;
-
-        GetCombination(array, result, target, index++, sum);
+        index++;
     }
-
-    
 
     return result;
 }
