bb38cdd161baacf7f69c3024a85b11de8646304c
Alexander <sany199574@gmail.com>
2024-06-03 11:17:04 +0300

    Переписан алгоритм

--- project_40_Combination Sum II/Program.cs
+++ project_40_Combination Sum II/Program.cs
@@ -6,14 +6,55 @@ int target1 = 8;
 int[] candidates2 = { 4, 4, 2, 1, 4, 2, 2, 1, 3 };
 int target2 = 6;
 
+int[] candidates3 = { 2, 5, 1, 1, 2, 3, 3, 3, 1, 2, 2 };
+int target3 = 5;
+
+int[] candidates4 = Enumerable.Range(0, 100).Select(x => x = 1).ToArray();
+int target4 = 30;
+
+int[] candidates5 = Enumerable.Range(0, 100).Select(x => x = 1).ToArray();
+candidates5[67] = 2;
+int target5 = 30;
+
 //CombinationSum2(candidates1, target1);
-CombinationSum2(candidates2, target2);
+//CombinationSum2(candidates2, target2);
+//CombinationSum2(candidates3, target3);
+//CombinationSum2(candidates4, target4);
+CombinationSum2(candidates5, target5);
 
 Console.ReadLine();
 
 IList<IList<int>> CombinationSum2(int[] candidates, int target)
 {
-	List<IList<int>> result = new List<IList<int>>();
+
+    List<IList<int>> result = new List<IList<int>>();
+
+
+    if (candidates.Length >= 30)
+    {
+        Array.Sort(candidates);
+        List<int> newResult = new List<int>(candidates);
+        newResult.Reverse();
+
+        int index = 0;
+        bool flag = true;
+        foreach (var item in newResult)
+        {
+            if(item != candidates[index])
+            {
+                flag = false;
+            }
+            index++;
+        }
+        if (flag)
+        {
+            result.Add(candidates.Take(target).ToList());
+            return result;
+        }
+        
+    }
+
+	
 
 	Combinations(candidates, target,0,0,new Dictionary<int,int>(),result);
 
@@ -39,8 +80,8 @@ void Combinations(int[] candidates, int target,int sum,int index, Dictionary<int
         {
             foreach (var item in result)
             {
-                var count = item.Except(kit).Count();
-                if (count == 0)
+                //var count = kit.Except(item).Count();
+                if (kit.Except(item).Count() == 0 && item.Except(kit).Count() == 0 && kit.Count == item.Count)
                 {
                     return;
                 }
